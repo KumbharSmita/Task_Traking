0df0f0c9badd99ced7358a58ae786dfea44909ad
Kumbhar Smita <kumbharsmita2000@gmail.com>
2023-10-25 02:47:06 +0530

    Add new feature

--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -29,25 +29,41 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from '../service/auth.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  constructor(private service: AuthService, private router: Router) {}
+  constructor(private service: AuthService,
+    private router: Router,
+    private toastr:ToastrService) {}
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     if (this.service.IsloggedIn()) {
-      return true;
+
+      if(route.url.length>0){
+        let menu=route.url[0].path;
+        if(menu=='user'){
+          if(this.service.GetUserRole()=='admin'){
+            return true;
+          }else{
+            this.toastr.warning('you dont have access');
+            this.router.navigate(['']);
+            return false;
+          }
+        }else{
+          return true;
+        }
+      }else{
+        return true;
+      }
     } else {
-      // If the user is not logged in, you can return a UrlTree to navigate to the login page.
-      // Alternatively, you can use the Router to navigate asynchronously.
-      // For example:
-      // this.router.navigate(['login']);
-      return this.router.parseUrl('/login'); // Navigate to the login page
+      return this.router.parseUrl('/login');
+      return false; // Navigate to the login page
     }
   }
 }
